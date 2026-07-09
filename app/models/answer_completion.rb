c4b22fd2aa240745524bbf99038fd70ecf1edb34
shi-chan1205 <cherry.rose1225.s@gmail.com>
2026-07-09 14:04:07 +0900

    SP2:answer_completionsの1日1回制約への設計変更

--- app/models/answer_completion.rb
+++ app/models/answer_completion.rb
@@ -1,4 +1,9 @@
 class AnswerCompletion < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :question_id, uniqueness: {
+    scope: [:user_id, :completed_on],
+    message: "は今日すでに完了しています"
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_07_194611) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_08_221146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,8 +47,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_07_194611) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "completed_on"
     t.index ["question_id"], name: "index_answer_completions_on_question_id"
-    t.index ["user_id", "question_id"], name: "index_answer_completions_on_user_id_and_question_id", unique: true
+    t.index ["user_id", "question_id", "completed_on"], name: "idx_on_user_id_question_id_completed_on_56dfb01271", unique: true
     t.index ["user_id"], name: "index_answer_completions_on_user_id"
   end
 
